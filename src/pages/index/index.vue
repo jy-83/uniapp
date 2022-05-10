70681c0885c54f77366c572354de6666a4925fe4
jiangyi <189363638@qq.com>
2022-05-10 17:32:14 +0800

    feat: scss集成

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,38 +1,19 @@
 <template>
-  <view>
+  <view class="text">
     <text>{{ title }}</text>
   </view>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+console.log(process.env.VUE_APP_BASE_URL);
 let title = ref("测试");
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+<style lang="scss" scoped>
+.text {
+  text {
+    color: red;
+  }
 }
 </style>
